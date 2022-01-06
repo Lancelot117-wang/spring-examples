d64a457137e75927e4ae3a0294d353124628d4dc
Junhan <junhanwang@umass.edu>
2022-01-06 08:56:55 -0600

    spring-boot-lombok-example

---
+++ spring-boot-lombok-example/src/main/java/com/lance/Application.java
@@ -0,0 +1,12 @@
+package com.lance;
+
+import com.lance.entity.UserDO;
+
+public class Application {
+
+    public static void main(String[] args) {
+        UserDO userDO = new UserDO();
+        userDO.getUsername();
+        userDO.getPassword();
+    }
+}
---
+++ spring-boot-lombok-example/src/main/java/com/lance/entity/UserDO.java
@@ -0,0 +1,13 @@
+package com.lance.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserDO {
+
+    private String username;
+    private String password;
+
+}
